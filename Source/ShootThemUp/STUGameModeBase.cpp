63ebdf08660d322f0f852849bf2613cc0471f84a
Artem Mazgo <mazgoAE@gmail.com>
2023-05-09 21:07:49 +0300

    [STU-23] Set up project

---
+++ Source/ShootThemUp/STUGameModeBase.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "STUGameModeBase.h"
+
+void ASTUGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+    int32 Variable = 0;
+    if (Variable < 0)
+    {
+        for (int32 i = 0; i < 10; i++)
+        {
+            UE_LOG(LogTemp, Warning, TEXT("index i: %i"), i);
+        }
+    }
+}
