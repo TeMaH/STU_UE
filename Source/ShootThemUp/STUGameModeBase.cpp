f7b714555aa3f7228bf21245acd0094e03e35107
Mazgo Artsiom <mazgoAE@gmail.com>
2024-01-22 21:44:24 +0300

    [STU-111] Spawn AI

--- Source/ShootThemUp/STUGameModeBase.cpp
+++ Source/ShootThemUp/STUGameModeBase.cpp
@@ -1,5 +1,6 @@
 #include "STUGameModeBase.h"
 
+#include "AIController.h"
 #include "STUGameHUD.h"
 
 #include "Player/STUBaseCharacter.h"
@@ -12,7 +13,37 @@ ASTUGameModeBase::ASTUGameModeBase()
     HUDClass = ASTUGameHUD::StaticClass();
 }
 
+void ASTUGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+    SpawnBots();
+}
+
 void ASTUGameModeBase::BeginPlay()
 {
     Super::BeginPlay();
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if(InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::SpawnBots()
+{
+    const auto World = GetWorld();
+
+    for(int32 i = 0; i < GameData.Players; i++)
+    {
+        FActorSpawnParameters SpawnParameters;
+        SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        const auto AIController = World->SpawnActor<AAIController>(AIControllerClass, SpawnParameters);
+
+        RestartPlayer(AIController);
+    }
 }
--- Source/ShootThemUp/STUGameModeBase.h
+++ Source/ShootThemUp/STUGameModeBase.h
@@ -7,6 +7,19 @@
 
 #include "STUGameModeBase.generated.h"
 
+class ASTUBaseCharacter;
+class AAIController;
+
+USTRUCT(BlueprintType)
+struct SHOOTTHEMUP_API FGameData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    int32 Players;
+};
+
+
 /**
  *
  */
@@ -17,6 +30,23 @@ class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 
 public:
     ASTUGameModeBase();
+
+    virtual void StartPlay() override;
+    
 protected:
     virtual void BeginPlay() override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    TSubclassOf<AAIController> AIControllerClass;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    TSubclassOf<ASTUBaseCharacter> AIPawnClass;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    FGameData GameData;
+
+    UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+private:
+    void SpawnBots();
 };
